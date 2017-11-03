ef9daa255a5da55040d4cbcaaac16c9e74ce1ec3
Denis Atkesone <denis.atkesone@gmail.com>
2017-11-03 11:25:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ def input_to_index(index = -1)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && (board[index] == " " || board[index] == "" || board[index] == nil)
+  if index.between?(0, 8)
     return true
   else
     return false
